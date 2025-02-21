fc4cba02b9bf4403018a3ca8b7b31ef5fb290d09
akyyev <akyyevtm@gmail.com>
2025-02-21 01:02:47 -0600

    refactor: rename project and update dependencies in package files

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,7 +6,7 @@ import '../assets/styles/Project.scss';
 function Project() {
     return(
     <div className="projects-container" id="projects">
-        <h1>Personal Projects</h1>
+        <h1>Projects</h1>
         <div className="projects-grid">
             <div className="project">
                 <a href="https://samsclub.com" target="_blank" rel="noreferrer"><img src={rye} className="zoom" alt="thumbnail" width="100%"/></a>
